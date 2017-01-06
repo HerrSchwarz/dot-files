0e003acd0335ea5764c9a9598cf37c0b4d8337ce
Felix Eckhardt <eckhardt@slpn-eckhardt.office.silpion.de>
2017-01-06 10:52:40 +0100

    create symlinks

--- link.sh
+++ link.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/zsh
-ln -s ./.zshrc ~/.zshrc 
-ln -s ./.zshrc.zni ~/.zshrc.zni 
+
+dot_files=(.zshrc .zshrc.zni)
+
+for file in $dot_files; do
+  if [ ! -L ~/$file ]; then
+    if [ -f ./$file ]; then
+      ln -s ./$file ~/$file
+    else
+      echo "file $file does not exists"
+    fi
+  else
+    echo "symlink for $file already exists"
+  fi 
+done
